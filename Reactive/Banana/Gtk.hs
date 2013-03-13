aad73a76caca4d8ff8ab03d0076319faea858b79
Christian Conkle <christian@conkle.org>
2013-03-13 08:49:39 -0700

    Update for reactive-banana 0.7* API, including Moment monad

--- Reactive/Banana/Gtk.hs
+++ Reactive/Banana/Gtk.hs
@@ -1,61 +1,89 @@
-{-# LANGUAGE ExistentialQuantification #-}
-
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
 module Reactive.Banana.Gtk (
-  AttrOp'(..), eventM, event0, event1, event2, event3,
-  behavior, sink, reactimateEventM
+  AttrBinding(..), eventM, event0, event1, event2, event3,
+  monitorAttr, pollAttr, sink
 ) where
 
-import Control.Monad.Reader
-import Foreign.Ptr
 import Reactive.Banana
-import System.Glib.Attributes
-import System.Glib.Signals
+import Reactive.Banana.Frameworks
+import System.Glib.Attributes (ReadWriteAttr, AttrOp((:=)))
+import qualified System.Glib.Attributes as Attrs
+import System.Glib.Signals (Signal, on, signalDisconnect)
 
 import qualified Graphics.UI.Gtk as Gtk
-import qualified Graphics.UI.Gtk.General.Enums as Gtk
 
-type EventBuilder self callback a =
-  self -> Signal self callback -> NetworkDescription (Event a)
+eventM :: (Frameworks t, Gtk.GObjectClass self)
+    => self
+    -> Signal self (Gtk.EventM a Bool)
+    -> Gtk.EventM a b
+    -> Moment t (Event t b)
+eventM self signal m = 
+    fromAddHandler $ \e -> do
+        callbackId <- on self signal $ m >> return False
+        return $ signalDisconnect callbackId
+            
+eventN :: (Frameworks t, Gtk.GObjectClass self) 
+    => ((a -> IO ()) -> callback) 
+    -> self
+    -> Signal self callback
+    -> Moment t (Event t a)
+eventN f self signal =
+    fromAddHandler $ \e -> do
+        let 
+            callback = f e
+        callbackId <- on self signal callback
+        return $ signalDisconnect callbackId
 
-eventM :: EventBuilder self (Gtk.EventM a Bool) (Ptr a)
-eventM self signal = do
-  (addHandler, runHandlers) <- liftIO newAddHandler
-  liftIO $ on self signal $ Gtk.tryEvent $ ask >>= liftIO . runHandlers
-  fromAddHandler addHandler
-
-eventN :: ((a -> IO ()) -> callback) -> EventBuilder self callback a
-eventN f self signal = do
-  (addHandler, runHandlers) <- liftIO newAddHandler
-  liftIO $ on self signal $ f runHandlers
-  fromAddHandler addHandler
-
-event0 :: EventBuilder self (IO ()) ()
+event0 :: (Frameworks t, Gtk.GObjectClass self) 
+    => self
+    -> Signal self (IO ())
+    -> Moment t (Event t ())
 event0 = eventN ($ ())
 
-event1 :: (Typeable a) => EventBuilder self (a -> IO ()) a
+event1 :: (Frameworks t, Gtk.GObjectClass self) 
+    => self
+    -> Signal self (a -> IO ())
+    -> Moment t (Event t a)
 event1 = eventN id
 
-event2 :: (Typeable a, Typeable b)
-       => EventBuilder self (a -> b -> IO ()) (a, b)
+event2 :: (Frameworks t, Gtk.GObjectClass self) 
+    => self
+    -> Signal self (a -> b -> IO ())
+    -> Moment t (Event t (a, b))
 event2 = eventN curry
 
-event3 :: (Typeable a, Typeable b, Typeable c)
-       => EventBuilder self (a -> b -> c -> IO ()) (a, b, c)
+event3 :: (Frameworks t, Gtk.GObjectClass self) 
+    => self
+    -> Signal self (a -> b -> c -> IO ())
+    -> Moment t (Event t (a, b, c))
 event3 = eventN $ \f a b c -> f (a, b, c)
 
-behavior :: self -> Attr self a -> NetworkDescription (Behavior a)
-behavior widget attr = fromPoll $ liftIO $ get widget attr
+monitorAttr :: 
+    (Frameworks t, Gtk.GObjectClass self)
+    => self
+    -> Signal self (IO ()) -- | Signal indicating when to read the attribute
+    -> ReadWriteAttr self a b
+    -> Moment t (Event t a)
+monitorAttr self signal attr =
+    fromAddHandler $ \e -> do
+        callbackId <- on self signal $ Attrs.get self attr >>= e >> return ()
+        return $ signalDisconnect callbackId
 
-data AttrOp' o = forall a. (Attr o a) :== Discrete a
+pollAttr :: (Frameworks t) => self -> ReadWriteAttr self a b -> Moment t (Behavior t a)
+pollAttr widget attr = fromPoll $ liftIO $ Attrs.get widget attr
+
+data AttrBinding t o = forall a b. (ReadWriteAttr o a b) :== Behavior t b
 
 infixr 0 :==
 
-sink :: self -> [AttrOp' self] -> NetworkDescription ()
-sink self props = mapM_ sink1 props
+sink :: (Frameworks t) => self -> [AttrBinding t self] -> Moment t ()
+sink self = mapM_ sink'
   where
-    sink1 (attr :== x) = do
-      liftIOLater $ set self [attr := initial x]
-      reactimate $ (\x -> set self [attr := x]) <$> changes x
+    sink' (attr :== x) = do
+      i <- initial x
+      xs <- changes x
+      liftIOLater $ Attrs.set self [attr := i]
+      reactimate $ (\x -> Attrs.set self [attr := x]) <$> xs
 
-reactimateEventM :: Event (Ptr a) -> Gtk.EventM a () -> NetworkDescription ()
-reactimateEventM event reader = reactimate $ (<$> event) $ runReaderT reader
+-- reactimateEventM :: Event (Ptr a) -> Gtk.EventM a () -> Moment ()
+-- reactimateEventM event reader = reactimate $ (<$> event) $ runReaderT reader
